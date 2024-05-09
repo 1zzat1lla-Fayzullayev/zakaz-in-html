42b6468db8d958b9e033c2c9ea41334896cc4ad5
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-09 10:32:41 +0500

    add rating

--- array/Authors.js
+++ array/Authors.js
@@ -4,18 +4,21 @@ const authors = [
 		description:
 			'“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.',
 		img: './assets/author1.png',
+		rating: './assets/rating5.png',
 	},
 	{
 		name: 'Yessica Christy',
 		description:
 			'“I like it because I like to travel far and still can connect with high speed.”.',
 		img: './assets/author2.png',
+		rating: './assets/rating4.png',
 	},
 	{
 		name: 'Kim Young Jou',
 		description:
 			'“This is very unusual for my business that currently requires a virtual private network that has high security.”.',
 		img: './assets/author3.png',
+		rating: './assets/rating5.png',
 	},
 ]
 
@@ -28,7 +31,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="mt-[30px] ml-[30px]">
                     <div class="flex items-center gap-[20px]">
                         <img src="${author.img}" alt="user" />
+						<div class="flex flex-col items-start">
                         <p class="text-[#0B132A] text-[18px] font-normal">${author.name}</p>
+                        <img src="${author.rating}" alt="rating" />
+						</div>
                     </div>
                     <p class="mt-[20px] text-[#0B132A] text-[16px] p-3">${author.description}</p>
                 </div>
